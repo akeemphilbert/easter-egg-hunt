f7abb5feeadf712010f1c7ef00f85235a2249eb5
Akeem Philbert <akeem.philbert@wepala.com>
2020-04-05 17:08:15 -0400

    Got the navigation working

--- App.js
+++ App.js
@@ -1,18 +1,30 @@
+import 'react-native-gesture-handler';
 import React, { Component } from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import {Provider} from "react-redux";
 //store
 import store from "./js/store";
 //screens
+import HomeScreen from  "./js/screens/Home"
 import CreateEggHunt from "./js/containers/CreateEggHunt"
+const Stack = createStackNavigator();
 
-export default class EggHuntApp extends Component {
+export default class App extends Component {
   render() {
     return (
         <Provider store={store}>
-          <CreateEggHunt />
+          <NavigationContainer>
+            <Stack.Navigator>
+              <Stack.Screen
+                  name="Home"
+                  component={HomeScreen}
+                  options={{title: 'Welcome'}}
+              />
+                <Stack.Screen name="CreateEggHunt" component={CreateEggHunt} />
+            </Stack.Navigator>
+          </NavigationContainer>
         </Provider>
     );
   }
 }
-
-module.exports = EggHuntApp
