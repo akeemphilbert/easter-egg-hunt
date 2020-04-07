793addd1e085dd3f86fc060a6d7cd8615cc329e4
Akeem Philbert <akeem.philbert@wepala.com>
2020-04-07 08:30:39 -0400

    EGG-69 Added Find Screen
    * Added Permissions for location
    * Added a status screen to show how much eggs was found
    * Added the AR screen for finding eggs
    * Got it to place some eggs on the screen
    * Removed the list of hunts from the home screen

--- App.js
+++ App.js
@@ -10,6 +10,9 @@ import HomeScreen from  "./js/screens/Home"
 import EggList from "./js/containers/EggList"
 import HideEgg from "./js/containers/HideEgg";
 import Invite from "./js/containers/Invite";
+import FindEgg from "./js/containers/FindEgg";
+import HuntStatus from "./js/containers/HuntStatus";
+
 const Stack = createStackNavigator();
 
 export default class App extends Component {
@@ -26,6 +29,8 @@ export default class App extends Component {
                 <Stack.Screen name="CreateEggHunt" component={EggList} />
                 <Stack.Screen name="HideEgg" component={HideEgg} />
                 <Stack.Screen name="Invite" component={Invite} />
+                <Stack.Screen name="FindEgg" component={FindEgg} />
+                <Stack.Screen name="HuntStatus" component={HuntStatus} />
             </Stack.Navigator>
           </NavigationContainer>
         </Provider>
