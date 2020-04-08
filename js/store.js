aaebec4a3d9bfbf3832bc2071bbd919d541ec666
Simon Sankar <simon.sankar@wepala.com>
2020-04-08 10:06:25 -0400

    EGG-34 removed dummy eggs in store

--- js/store.js
+++ js/store.js
@@ -14,51 +14,6 @@ const initialState = {
       title: 'egg 2',
       image: 'some image',
     },
-    '2bd4b823-1beb-4468-b150-d93a1d6ae117': {
-      id: '2bd4b823-1beb-4468-b150-d93a1d6ae117',
-      title: 'egg 3',
-      image: 'some image',
-    },
-    '3bd4b823-1beb-4468-b150-d93a1d6ae117': {
-      id: '3bd4b823-1beb-4468-b150-d93a1d6ae117',
-      title: 'egg 4',
-      image: 'some image',
-    },
-    '4bd4b823-1beb-4468-b150-d93a1d6ae117': {
-      id: '3bd4b823-1beb-4468-b150-d93a1d6ae117',
-      title: 'egg 5',
-      image: 'some image',
-    },
-    '5bd4b823-1beb-4468-b150-d93a1d6ae117': {
-      id: '3bd4b823-1beb-4468-b150-d93a1d6ae117',
-      title: 'egg 6',
-      image: 'some image',
-    },
-    '6bd4b823-1beb-4468-b150-d93a1d6ae117': {
-      id: '3bd4b823-1beb-4468-b150-d93a1d6ae117',
-      title: 'egg 7',
-      image: 'some image',
-    },
-    '7bd4b823-1beb-4468-b150-d93a1d6ae117': {
-      id: '3bd4b823-1beb-4468-b150-d93a1d6ae117',
-      title: 'egg 7',
-      image: 'some image',
-    },
-    '8bd4b823-1beb-4468-b150-d93a1d6ae117': {
-      id: '3bd4b823-1beb-4468-b150-d93a1d6ae117',
-      title: 'egg 7',
-      image: 'some image',
-    },
-    '9bd4b823-1beb-4468-b150-d93a1d6ae117': {
-      id: '3bd4b823-1beb-4468-b150-d93a1d6ae117',
-      title: 'egg 7',
-      image: 'some image',
-    },
-    '10d4b823-1beb-4468-b150-d93a1d6ae117': {
-      id: '3bd4b823-1beb-4468-b150-d93a1d6ae117',
-      title: 'egg 7',
-      image: 'some image',
-    },
   },
 };
 const middlewares = [thunk];
